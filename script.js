7fdecdf5dac8deb6cc7f1eb1da4b45560788818d
Astha47 <anasfathurrahman.edu@gmail.com>
2023-05-12 22:24:35 +0700

    ui update

--- script.js
+++ script.js
@@ -51,11 +51,35 @@ function typeText(element, texts, delay, pause, fadeInDelay, textIndex) {
   setTimeout(type, fadeInDelay);
 }
 
+
 var counter = 1;
+
 setInterval(function(){
-  document.getElementById('slide'+counter).checked = true;
+
+  
+  var slide1 = document.getElementById("slide1");
+  var slide2 = document.getElementById("slide2");
+  var slide3 = document.getElementById("slide3");
+  var slide4 = document.getElementById("slide4");
+
+  if (slide1.checked == true) {
+    counter = 1
+  } else if(slide2.checked == true) {
+    counter = 2
+  } else if(slide3.checked == true) {
+    counter = 3
+  } else if(slide4.checked == true) {
+    counter = 4
+  }
+
+  console.log("counter sekarang = ",counter)
+  
   counter++;
   if (counter > 4) {
     counter= 1
   }
-}, 7000)
+
+  document.getElementById('slide'+counter).checked = true;
+
+  console.log("counter setelahnya = ",counter)
+}, 10000)
